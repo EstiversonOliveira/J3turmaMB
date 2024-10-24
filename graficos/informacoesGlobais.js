d633df651e5df082c5b2c53799f4b96510fede27
Estíverson Oliveira <107802352+EstiversonOliveira@users.noreply.github.com>
2024-10-24 09:35:51 -0300

    final R4

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,9 +1,16 @@
-const url= "https://github.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/blob/Aula01/transporte/transporte-dados-globais.json"
+const url="https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json";
 
 async function visualizarInformacoesGlobais (){
     const resposta = await fetch (url);
-    const dados = await resposta.json;
+    const dados = await resposta.json();
     console.log(dados);
+
+    const paragrafo = document.createElement("p");
+    paragrafo.classList.add("graficos-container__texto");
+    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas no mundo?.`;
+    
+    const container = document.getElementById('graficos-container');
+    container.appendChild(paragrafo);
 }
 
 visualizarInformacoesGlobais();
