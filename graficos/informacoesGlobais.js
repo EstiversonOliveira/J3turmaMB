6686ec3abfd6a310b5122ec816e35029d1ef4333
Estíverson Oliveira <107802352+EstiversonOliveira@users.noreply.github.com>
2024-10-31 10:35:48 -0300

    updated final da Reunião 5

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -7,7 +7,7 @@ async function visualizarInformacoesGlobais (){
 
     const paragrafo = document.createElement("p");
     paragrafo.classList.add("graficos-container__texto");
-    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas no mundo?.`;
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${dados.total_pessoas_mundo} </span> de pessoas no mundo?.`;
     
     const container = document.getElementById('graficos-container');
     container.appendChild(paragrafo);
